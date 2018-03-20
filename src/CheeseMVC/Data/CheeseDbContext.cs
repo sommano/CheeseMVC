ef4cc18925dc45b3e8285243b36ac2f268ef01e6
sommano phivilay <sommano@gmail.com>
2018-03-20 15:54:01 -0400

    first commit

---
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CheeseMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Data
+{
+    public class CheeseDbContext : DbContext
+    {
+        public DbSet<Cheese> Cheeses { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            // Specify the path of the database here
+            optionsBuilder.UseSqlite("Data Source=./CheeseMVC.sqlite");
+        }
+
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
+            : base(options)
+        { }
+
+    }
+}
